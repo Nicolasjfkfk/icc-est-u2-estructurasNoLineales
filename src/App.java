19c1d79be3a3889e70f88c8d2f00d18df343db26
Nicolas <nico01pomaquiza@gmail.com>
2025-01-08 12:43:56 -0500

    completado

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import main.Materia.Controllers.ArbolBinario;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        runArbolBinario();
+    } 
+    public static void runArbolBinario(){
+        ArbolBinario runArbolBinario = new ArbolBinario();
+        int[] valores = {40, 20, 60, 10, 30, 50, 70, 5, 15, 55};
+
+        for (int valor : valores) {
+            runArbolBinario.insert(valor);
+        }
+        runArbolBinario.printTree();
+    }
+}
